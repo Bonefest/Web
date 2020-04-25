23ce190d725ceb5f93e57a6de1993d23fb48c522
Bonefest <tereshenko1@ukr.net>
2020-04-25 18:12:10 +0300

    templates inheritance

--- laravel/resources/views/item_info.blade.php
+++ laravel/resources/views/item_info.blade.php
@@ -1,80 +1,69 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('item_parent')
+@section('css')
+html, body {
+    background-color: #fff;
+    color: #636b6f;
+    font-family: 'Nunito', sans-serif;
+    font-weight: 200;
+    height: 100vh;
+    margin: 0;
+}
 
-        <title>Item list</title>
+.title {
+    font-size: 84px;
+    margin: 0px;
+}
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+.items {
+    color: #636b6f;
+    padding: 0 25px;
+    font-size: 13px;
+    font-weight: 600;
+    letter-spacing: .1rem;
+    text-decoration: none;
+    text-transform: uppercase;
+}
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+.information {
+    margin-bottom: 50px;
+}
 
-            .title {
-                font-size: 84px;
-                margin: 0px;
-            }
+.items > a{
+    color: #3b3c3d;
+    font-size: 13px;
+    font-weight: 600;
+    letter-spacing: .1rem;
+    text-decoration: none;
+    text-transform: uppercase;
+}
 
-            .items {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+.m-b-md {
+    margin-bottom: 30px;
+}
+@endsection
 
-            .information {
-                margin-bottom: 50px;
-            }
+@section('title')
+    {{$item->name}}
+@stop
 
-            .items > a{
-                color: #3b3c3d;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class = "items">
-            <div class="title m-b-md">
-                {{$item->name}}
-             </div>
+@section('body')
+    <div class = "items">
+        <div class="title m-b-md">
+            {{$item->name}}
+         </div>
 
 
-            <div class = "information">
-                <h3>
-                    {{$item->information}}
-                </h3>
-            </div>
-
-
-            <h4>
-                Price: {{$item->price}}$
-            </h4>
-
-            <a href="/laravel_learning/public/">[Return]</a>
+        <div class = "information">
+            <h3>
+                {{$item->information}}
+            </h3>
         </div>
 
- 
-      
-    </body>
-</html>
+
+        <h4>
+            Price: {{$item->price}}$
+        </h4>
+
+        <a href="/laravel_learning/public/">[Return]</a>
+    </div>
+@endsection
--- laravel/resources/views/item_list.blade.php
+++ laravel/resources/views/item_list.blade.php
@@ -1,123 +1,114 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('item_parent')
+@section('css')
+html, body {
+    background-color: rgb(250, 250, 250);
+    color: #636b6f;
+    font-family: 'Nunito', sans-serif;
+    font-weight: 200;
+    height: 100vh;
+    margin: 0;
+}
 
-        <title>Item list</title>
+.title {
+    font-size: 84px;
+}
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+.delete_button input {
+    width: 10%;
+    padding: 10px 5px 10px 5px;
+    border-radius: 0px;
+    border: 0px;
+    background-color: rgb(217, 33, 76);
+    margin-bottom: 20px;
+    margin-left: 20px;
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: rgb(250, 250, 250);
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    font-size: 18px;
+    color: #EAEAEA;
+}
 
-            .title {
-                font-size: 84px;
-            }
+.item {
+    background-color: #EAEAEA;
+    margin-bottom: 5px;
+    box-shadow: 0px 1.5px rgb(200, 200, 200);
+}
 
-            .delete_button input {
-                width: 10%;
-                padding: 10px 5px 10px 5px;
-                border-radius: 0px;
-                border: 0px;
-                background-color: rgb(217, 33, 76);
-                margin-bottom: 20px;
-                margin-left: 20px;
-
-                font-size: 18px;
-                color: #EAEAEA;
-            }
-
-            .item {
-                background-color: #EAEAEA;
-                margin-bottom: 5px;
-                box-shadow: 0px 1.5px rgb(200, 200, 200);
-            }
-
-            .item > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+.item > a {
+    color: #636b6f;
+    padding: 0 25px;
+    font-size: 13px;
+    font-weight: 600;
+    letter-spacing: .1rem;
+    text-decoration: none;
+    text-transform: uppercase;
+}
 
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
+.m-b-md {
+    margin-bottom: 30px;
+}
 
-            .create_item {
+.create_item {
 
-                background-color: rgba(240, 240, 240, 10);
-                text-align: center;
-            }
+    background-color: rgba(240, 240, 240, 10);
+    text-align: center;
+}
 
-            .create_item input {
-                    width: 10%;
-                    padding: 10px 5px 10px 5px;
-                    border-radius: 0px;
-                    border: 2px solid #999;
-                    background-color: rgba(240, 240, 240, 255);
-                    margin-bottom: 20px;
-            }
-        </style>
-    </head>
-    <body>
-        @if(count($items) > 0)
-            <div class="title m-b-md">
-                Item list
-             </div>
+.create_item input {
+        width: 10%;
+        padding: 10px 5px 10px 5px;
+        border-radius: 0px;
+        border: 2px solid #999;
+        background-color: rgba(240, 240, 240, 255);
+        margin-bottom: 20px;
+}
+@endsection
 
-            <form id = "delete" method="post">
-                @foreach ($items as $item)
-                   
-                    <div class = "item">
-                        <input type = "checkbox" name="checkbox[]" id = "delete" value="{{$item->id}}">
-                        <a href="item/{{$item->id}}">{{$item->name}}: {{$item->price}}$</a>
-                    </div>
-                @endforeach
+@section('title')
+    Item list
+@stop
 
-                <div class = "delete_button">
-                    <input type="submit" value="Delete">
-                </div>
+@section('body')
+@if(count($items) > 0)
+    <div class="title m-b-md">
+        Item list
+     </div>
 
-                <input type="hidden" name = "post_type" value = "delete_items">        
-            </form>
-        @else
-            <div class="title m-b-md">
-                Items not found!
+    <form id = "delete" method="post">
+        @foreach ($items as $item)
+           
+            <div class = "item">
+                <input type = "checkbox" name="checkbox[]" id = "delete" value="{{$item->id}}">
+                <a href="item/{{$item->id}}">{{$item->name}}: {{$item->price}}$</a>
             </div>
+        @endforeach
 
-        @endif
-
-        <div class = "create_item">
-
-            <h2>Create new item </h2>
-
-            <form id = "create" method="post">
-                  <label for="item_name">Item name</label><br>
-                  <input type="text" id = "item_name" name="item_name" placeholder="Type name"><br>
-
-                  <label for="item_price">Item price</label><br>
-                  <input type="number" id = "item_price" name="item_price" placeholder="Type price"><br>
-
-                  <input type="submit" value="Create">
-
-                  <input type="hidden" name = "post_type" value = "create_item">
-            </form>
+        <div class = "delete_button">
+            <input type="submit" value="Delete">
         </div>
-      
-    </body>
-</html>
+
+        <input type="hidden" name = "post_type" value = "delete_items">        
+    </form>
+@else
+    <div class="title m-b-md">
+        Items not found!
+    </div>
+
+@endif
+
+<div class = "create_item">
+
+    <h2>Create new item </h2>
+
+    <form id = "create" method="post">
+          <label for="item_name">Item name</label><br>
+          <input type="text" id = "item_name" name="item_name" placeholder="Type name"><br>
+
+          <label for="item_price">Item price</label><br>
+          <input type="number" id = "item_price" name="item_price" placeholder="Type price"><br>
+
+          <input type="submit" value="Create">
+
+          <input type="hidden" name = "post_type" value = "create_item">
+    </form>
+</div>
+@endsection
